85eca80d01ae5a3bbedf3ee29d628a75e061ba60
Rahadian Zahri Irwansyah <rzirwansyah@gmail.com>
2020-07-14 11:42:27 +0700

    menyelesaikan kontroller admin

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -63,3 +63,53 @@ const createAdmin = async (req, res) => {
     }
 
 };
+
+/**
+ * update user ke admin
+ * @param { object } req
+ * @param { object } res
+ * @returns { object } updated user
+ */
+
+const updateUserToAdmin = async (req, res) => {
+
+    const { id } = req.params;
+    const { isAdmin } = req.body;
+    const { is_admin } = req.user;
+
+    if (!is_admin === true) {
+        errorMessage.error = 'Sorry You are unauthorized to make a user an admin';
+        return res.status(status.bad).send(errorMessage);
+    }
+
+    if (isAdmin === '') {
+        errorMessage.error = 'Admin status is needed';
+        return res.status(status.bad).send(errorMessage);
+    }
+
+    const findUserQuery = 
+    `SELECT * FROM users WHERE id = $1`;
+    const updateUser = 
+    `UPDATE users SET is_admin = $1 WHERE id = $2 returning *`;
+
+    try {
+        const { rows } = await dbQuery.query(findUserQuery, [id]);
+        const dbResponse = rows[0];
+        if (!dbResponse) {
+            errorMessage.error = 'User cannot be found';
+            return res.status(status.notfound).send(errorMessage);
+        }
+        const values = [isAdmin, id,];
+        const response = await dbQuery.query(updateUser, values);
+        const dbResult = response.rows[0];
+        delete dbResult.password;
+        successMessage.data = dbResult;
+        return res.status(status.success).send(successMessage);
+    } catch (error) {
+        errorMessage.error = 'Operation was not successful';
+        return res.status(status.error).send(errorMessage);
+    }
+
+};
+
+export { createAdmin, updateUserToAdmin };
